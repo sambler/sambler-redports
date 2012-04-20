30bcf7c26ff9a23b897c35127c85260cebfcf1cd
sambler <sambler@2e334835-ea15-e111-ad42-00505682000c>
2012-04-20 07:19:24 +0000

    Update to RC1
    Redports: ignore
    
    
    git-svn-id: https://svn.redports.org/sambler@3192 2e334835-ea15-e111-ad42-00505682000c

--- graphics/luxrays/files/patch-cmake__PlatformSpecific.cmake
+++ graphics/luxrays/files/patch-cmake__PlatformSpecific.cmake
@@ -1,13 +1,12 @@
---- ./cmake/PlatformSpecific.cmake.orig	2012-02-26 09:20:53.000000000 +1030
-+++ ./cmake/PlatformSpecific.cmake	2012-03-02 13:59:29.000000000 +1030
-@@ -106,8 +106,8 @@
+--- cmake/PlatformSpecific.cmake.orig	2012-04-19 22:03:34.000000000 +0930
++++ cmake/PlatformSpecific.cmake	2012-04-20 15:34:52.000000000 +0930
+@@ -105,7 +105,7 @@
+ IF(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
    # Update if necessary
    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-long-long -pedantic")
- 
--  SET(CMAKE_CXX_FLAGS_DEBUG "-Wall -msse -msse2 -msse3 -mssse3 -fPIC -O0 -g")
--  SET(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -Wall -fPIC -O3 -ftree-vectorize -msse -msse2 -msse3 -mssse3 -fvariable-expansion-in-unroller")
-+#  SET(CMAKE_CXX_FLAGS_DEBUG "-Wall -msse -msse2 -msse3 -mssse3 -fPIC -O0 -g")
-+#  SET(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -Wall -fPIC -O3 -ftree-vectorize -msse -msse2 -msse3 -mssse3 -fvariable-expansion-in-unroller")
-   
- ENDIF()
+-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse -msse2 -msse3 -mssse3")
++#  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse -msse2 -msse3 -mssse3")
+   IF(NOT CYGWIN)
+ 	  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+   ENDIF(NOT CYGWIN)
  
