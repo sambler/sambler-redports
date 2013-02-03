332e5421f71645c5cabb0b05e55740474b3d3031
sambler <Develop@Shaneware.Biz>
2013-02-03 17:52:25 +1030

    opencolorio openimageio adjust patches to match submission sent upstream

--- graphics/openimageio/files/patch-include__thread.h
+++ graphics/openimageio/files/patch-include__thread.h
@@ -1,30 +1,13 @@
---- ./include/thread.h.orig	2012-11-10 05:51:18.000000000 +1030
-+++ ./include/thread.h	2012-11-11 17:25:34.547296939 +1030
-@@ -70,9 +70,14 @@
- #pragma GCC diagnostic error "-Wunused-variable"
+--- include/thread.h.orig	2013-01-10 11:43:37.000000000 +1030
++++ include/thread.h	2013-02-03 16:52:29.991857116 +1030
+@@ -98,8 +98,8 @@
  #endif
  
-+#if defined(__GNUC__) && (defined(_GLIBCXX_ATOMIC_BUILTINS) || (__GNUC__ * 100 + __GNUC_MINOR__ >= 401))
-+#define USE_GCC_ATOMICS 1
-+#undef USE_TBB
-+#else
- #ifndef USE_TBB
- #  define USE_TBB 0
- #endif
-+#endif
- 
- // Include files we need for atomic counters.
- // Some day, we hope this is all replaced by use of std::atomic<>.
-@@ -97,12 +102,6 @@
- #  include <libkern/OSAtomic.h>
- #endif
- 
--#if defined(__GNUC__) && (defined(_GLIBCXX_ATOMIC_BUILTINS) || (__GNUC__ * 100 + __GNUC_MINOR__ >= 401))
+ #if defined(__GNUC__) && (defined(_GLIBCXX_ATOMIC_BUILTINS) || (__GNUC__ * 100 + __GNUC_MINOR__ >= 401))
 -#if !defined(__FreeBSD__) || defined(__x86_64__)
 -#define USE_GCC_ATOMICS
--#endif
--#endif
--
- OIIO_NAMESPACE_ENTER
- {
++#if !USE_TBB
++#define USE_GCC_ATOMICS 1
+ #endif
+ #endif
  
