964fa006aa5e983802f518ee982a575b935be44a
sambler <Develop@Shaneware.Biz>
2013-11-25 02:34:13 +1030

    blender-* move install font to within nls test break build on 8.x - unknown cause cleanup patches from last update

--- graphics/blender267/files/patch-extern_carve_lib_triangulator.cpp
+++ graphics/blender267/files/patch-extern_carve_lib_triangulator.cpp
@@ -1,14 +1,14 @@
 --- extern/carve/lib/triangulator.cpp.orig	2012-01-30 09:45:12.000000000 +0100
 +++ extern/carve/lib/triangulator.cpp	2013-11-13 21:17:16.000000000 +0100
-@@ -122,8 +122,10 @@
+@@ -122,7 +122,9 @@
      std::vector<vertex_info *> queue;
  
      void checkheap() {
 -#ifdef __GNUC__
-+#if defined(__GNUC__) && !defined(__clang__)
-       CARVE_ASSERT(std::__is_heap(queue.begin(), queue.end(), vertex_info_ordering()));
-+#elif defined(__clang__) && defined(_LIBCPP_VERSION)
++#if defined(__clang__) && defined(_LIBCPP_VERSION)
 +      CARVE_ASSERT(std::is_heap(queue.begin(), queue.end(), vertex_info_ordering()));
++#elif defined(__GNUC__)
+       CARVE_ASSERT(std::__is_heap(queue.begin(), queue.end(), vertex_info_ordering()));
  #endif
      }
- 
+
