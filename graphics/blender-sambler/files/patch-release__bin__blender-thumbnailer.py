123cf150fbf6df2555b1518d18db15bc5f8ad6b5
sambler <Develop@Shaneware.Biz>
2014-03-03 20:56:46 +1030

    blender-sambler update to 2014-3-3

--- graphics/blender-sambler/files/patch-release__bin__blender-thumbnailer.py
+++ graphics/blender-sambler/files/patch-release__bin__blender-thumbnailer.py
@@ -1,11 +1,13 @@
---- ./release/bin/blender-thumbnailer.py.orig	2013-01-01 20:58:39.949174277 +1030
-+++ ./release/bin/blender-thumbnailer.py	2013-01-01 20:59:02.127178116 +1030
-@@ -24,7 +24,7 @@
- Thumbnailer runs with python 2.6 and 3.x.
- To run automatically with nautilus:
-    gconftool --type boolean --set /desktop/gnome/thumbnailers/application@x-blender/enable true
--   gconftool --type string --set /desktop/gnome/thumbnailers/application@x-blender/command "blender-thumbnailer.py %u %o"
-+   gconftool --type string --set /desktop/gnome/thumbnailers/application@x-blender/command "blender-thumbnailer-%%VERS_TAG%%.py %u %o"
+--- ./release/bin/blender-thumbnailer.py.orig	2014-03-03 20:46:09.180186531 +1030
++++ ./release/bin/blender-thumbnailer.py	2014-03-03 20:50:50.722187108 +1030
+@@ -28,8 +28,8 @@
+ with the following contents:
+ 
+ [Thumbnailer Entry]
+-TryExec=blender-thumbnailer.py
+-Exec=blender-thumbnailer.py %u %o
++TryExec=blender-thumbnailer-%%VERS_TAG%%.py
++Exec=blender-thumbnailer-%%VERS_TAG%%.py %u %o
+ MimeType=application/x-blender;
  """
  
- import struct
