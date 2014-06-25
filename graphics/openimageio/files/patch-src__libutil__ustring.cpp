06f062402f4b4a3a4d673407476ee522154732b5
sambler <Develop@Shaneware.Biz>
2014-06-25 20:48:59 +0930

    openimageio update patch to match upstream change

--- graphics/openimageio/files/patch-src__libutil__ustring.cpp
+++ graphics/openimageio/files/patch-src__libutil__ustring.cpp
@@ -1,15 +1,21 @@
---- ./src/libutil/ustring.cpp.orig	2014-05-24 01:51:53.000000000 +0930
-+++ ./src/libutil/ustring.cpp	2014-06-07 22:05:16.418536133 +0930
-@@ -67,8 +67,13 @@
+--- ./src/libutil/ustring.cpp.orig	2014-06-07 13:38:39.000000000 +0930
++++ ./src/libutil/ustring.cpp	2014-06-24 14:13:44.743536077 +0930
+@@ -67,8 +67,20 @@
  typedef null_lock<null_mutex> ustring_write_lock_t;
  #endif
  
--
-+#if defined(__FreeBSD__) && defined(__i386__) && !defined(__clang__)
-+// on a 32bit build using gcc4.2 make_unique() seg faults with the
-+// concurrent map enabled when used by openshadinglanguage
++#if defined(__i386__) && !defined(__clang__) && !defined(_MSC_VER)
++#if ((10000*__GNUC__ + 100*__GNUC_MINOR__ + __GNUC_PATCHLEVEL__) < 40300)
++// On a 32bit build using gcc4.2, make_unique() seg faults with the
++// concurrent map enabled, so turn it off. More recent gcc seems ok. That
++// old a gcc on 32 bit systems is a pretty rare combination, so we're not
++// going to sweat the lower performance of turning off the concurrent map
++// for that increasingly rare case.
 +#define USE_CONCURRENT_MAP 0
-+#else
++#endif
++#endif
+ 
++#ifndef USE_CONCURRENT_MAP
  #define USE_CONCURRENT_MAP 1
 +#endif
  
