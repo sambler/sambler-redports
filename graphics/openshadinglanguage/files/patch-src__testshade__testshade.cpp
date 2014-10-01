3060fcd01933a946669ba3e8adb5efec8fa06b37
sambler <Develop@Shaneware.Biz>
2014-10-01 16:49:02 +0930

    openshadinglanguage update to 1.5.11

--- graphics/openshadinglanguage/files/patch-src__testshade__testshade.cpp
+++ graphics/openshadinglanguage/files/patch-src__testshade__testshade.cpp
@@ -5,7 +5,7 @@
          shadingsys->getattribute (shadergroup.get(), "num_layers", num_layers);
          if (num_layers > 0) {
 -            std::vector<const char *> layers (num_layers, NULL);
-+            std::vector<const char *> layers ((size_t)num_layers, NULL);
++            std::vector<const char *> layers (size_t(num_layers), NULL);
              shadingsys->getattribute (shadergroup.get(), "layer_names",
                                        TypeDesc(TypeDesc::STRING, num_layers),
                                        &layers[0]);
