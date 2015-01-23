93d76f7a006ea398b79f9d8357be5a9ca37d6418
sambler <Develop@Shaneware.Biz>
2015-01-23 19:29:17 +1030

    godot remove clang settings - rely on environment use compiler to set compiler specific settings

--- devel/godot/files/patch-platform_x11_detect.py
+++ devel/godot/files/patch-platform_x11_detect.py
@@ -1,5 +1,5 @@
---- platform/x11/detect.py.orig	2014-12-15 18:49:18 UTC
-+++ platform/x11/detect.py
+--- platform/x11/detect.py.orig	2014-12-16 05:19:18.000000000 +1030
++++ platform/x11/detect.py	2015-01-23 16:08:21.497280349 +1030
 @@ -1,13 +1,13 @@
  
  import os
@@ -17,7 +17,7 @@
  
  
  def can_build():
-@@ -29,10 +29,10 @@ def can_build():
+@@ -29,10 +29,10 @@
  		print("X11 not found.. x11 disabled.")
  		return False
  
@@ -32,7 +32,7 @@
  
  	x11_error=os.system("pkg-config xcursor --modversion > /dev/null ")
  	if (x11_error):
-@@ -55,7 +55,7 @@ def get_flags():
+@@ -55,7 +55,7 @@
  	('builtin_zlib', 'no'),
  	("openssl", "yes"),
  	("theora","no"),
@@ -41,30 +41,35 @@
  			
  
  
-@@ -72,9 +72,10 @@ def configure(env):
+@@ -72,17 +72,16 @@
  
  	env.Append(CPPPATH=['#platform/x11'])
  	if (env["use_llvm"]=="yes"):
 -		env["CC"]="clang"
 -		env["CXX"]="clang++"
 -		env["LD"]="clang++"
+-		if (env["use_sanitizer"]=="yes"):
+-			env.Append(CXXFLAGS=['-fsanitize=address','-fno-omit-frame-pointer'])
+-			env.Append(LINKFLAGS=['-fsanitize=address'])
+-			env.extra_suffix=".llvms"
+-		else:
+-			env.extra_suffix=".llvm"
+-
+-
 +		if 'clang++' not in env['CXX']:
 +			env["CC"]="clang"
 +			env["CXX"]="clang++"
 +			env["LD"]="clang++"
- 		if (env["use_sanitizer"]=="yes"):
- 			env.Append(CXXFLAGS=['-fsanitize=address','-fno-omit-frame-pointer'])
- 			env.Append(LINKFLAGS=['-fsanitize=address'])
-@@ -82,6 +83,8 @@ def configure(env):
- 		else:
- 			env.extra_suffix=".llvm"
- 
-+	if ("clang++" in env["CXX"]):
-+		env.Append(CPPFLAGS=['-DTYPED_METHOD_BIND'])
++			env.Append(CPPFLAGS=['-DTYPED_METHOD_BIND'])
++		env.extra_suffix=".llvm"
++
++	if (env["use_sanitizer"]=="yes"):
++		env.Append(CCFLAGS=['-fsanitize=address','-fno-omit-frame-pointer'])
++		env.Append(LINKFLAGS='-fsanitize=address')
  
  
- 
-@@ -128,11 +131,6 @@ def configure(env):
+ 	#if (env["tools"]=="no"):
+@@ -128,11 +127,6 @@
  		env.Append(LINKFLAGS=['-m64','-L/usr/lib/i686-linux-gnu'])
  
  
