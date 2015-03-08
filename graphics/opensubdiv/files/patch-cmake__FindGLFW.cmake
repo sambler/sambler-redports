347c700736f12927fdcf5e61ed83c00e48a844ee
sambler <Develop@Shaneware.Biz>
2015-03-09 00:16:13 +1030

    graphics/opensubdiv update to 2.6.1

--- graphics/opensubdiv/files/patch-cmake__FindGLFW.cmake
+++ graphics/opensubdiv/files/patch-cmake__FindGLFW.cmake
@@ -1,16 +1,19 @@
---- ./cmake/FindGLFW.cmake.orig	2014-04-22 04:58:17.000000000 +0930
-+++ ./cmake/FindGLFW.cmake	2014-05-20 02:57:05.500230396 +0930
-@@ -119,8 +119,12 @@
-         if(NOT X11_xf86vmode_FOUND)
+--- ./cmake/FindGLFW.cmake.orig	2015-03-08 22:51:34.522337786 +1030
++++ ./cmake/FindGLFW.cmake	2015-03-08 22:51:57.095335165 +1030
+@@ -120,11 +120,15 @@
              message(FATAL_ERROR "xf86vmode library not found - required for GLFW")
          endif()
-+        
+ 
 +        if(NOT X11_Xi_FOUND)
 +            message(FATAL_ERROR "Xi library not found - required for GLFW")
 +        endif()
++
+         if(NOT X11_Xcursor_FOUND)
+             message(FATAL_ERROR "Xcursor library not found - required for GLFW")
+         endif()
  
--        list(APPEND GLFW_x11_LIBRARY "${X11_Xrandr_LIB}" "${X11_Xxf86vm_LIB}")
-+        list(APPEND GLFW_x11_LIBRARY "${X11_Xrandr_LIB}" "${X11_Xxf86vm_LIB}" "${X11_Xi_LIB}")
+-        list(APPEND GLFW_x11_LIBRARY "${X11_Xrandr_LIB}" "${X11_Xxf86vm_LIB}" "${X11_Xcursor_LIB}" -lrt)
++        list(APPEND GLFW_x11_LIBRARY "${X11_Xrandr_LIB}" "${X11_Xxf86vm_LIB}" "${X11_Xcursor_LIB}" "${X11_Xi_LIB}" -lrt)
  
          find_library( GLFW_glfw_LIBRARY
              NAMES 
