694d3686f3e70b0e31a4023eac8b370d801483bf
sambler <Develop@Shaneware.Biz>
2015-06-08 13:49:07 +0930

    opencolorio update to use oiio 1.5 adjust patches to keep portlint happy

--- graphics/opencolorio/files/patch-src_core_Platform.h
+++ graphics/opencolorio/files/patch-src_core_Platform.h
@@ -1,6 +1,6 @@
---- ./src/core/Platform.h.orig	2012-04-18 09:38:16.000000000 +0930
-+++ ./src/core/Platform.h	2012-10-08 00:20:27.668352332 +1030
-@@ -85,7 +85,9 @@
+--- src/core/Platform.h.orig	2012-12-11 23:21:23 UTC
++++ src/core/Platform.h
+@@ -85,7 +85,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  
  // linux/unix/posix
  #include <stdlib.h>
