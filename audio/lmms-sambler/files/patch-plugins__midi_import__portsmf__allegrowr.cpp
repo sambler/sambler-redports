27fd05777599d524f6f92352e0aa341d1c149beb
sambler <Develop@Shaneware.Biz>
2015-06-18 15:56:53 +0930

    lmms update for removal of portaudio2

--- audio/lmms-sambler/files/patch-plugins__midi_import__portsmf__allegrowr.cpp
+++ audio/lmms-sambler/files/patch-plugins__midi_import__portsmf__allegrowr.cpp
@@ -1,5 +1,5 @@
---- ./plugins/midi_import/portsmf/allegrowr.cpp.orig	2014-08-29 05:37:23.442454865 +0930
-+++ ./plugins/midi_import/portsmf/allegrowr.cpp	2014-08-29 05:37:23.607455215 +0930
+--- ./plugins/MidiImport/portsmf/allegrowr.cpp.orig	2014-08-29 05:37:23.442454865 +0930
++++ ./plugins/MidiImport/portsmf/allegrowr.cpp	2014-08-29 05:37:23.607455215 +0930
 @@ -41,7 +41,7 @@
          break;
      case 's': {
