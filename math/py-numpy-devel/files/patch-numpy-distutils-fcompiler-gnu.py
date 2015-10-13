35c4c0b54f983e27086927b03147f70a8ec1de09
sambler <Develop@Shaneware.Biz>
2015-10-14 00:41:04 +1030

    numpy-devel update to 1.10.1

--- math/py-numpy-devel/files/patch-numpy-distutils-fcompiler-gnu.py
+++ math/py-numpy-devel/files/patch-numpy-distutils-fcompiler-gnu.py
@@ -1,29 +1,35 @@
---- numpy/distutils/fcompiler/gnu.py.orig	2007-11-07 16:05:14.000000000 -0600
-+++ numpy/distutils/fcompiler/gnu.py	2008-04-07 13:54:56.000000000 -0500
-@@ -48,7 +48,7 @@
-     # Redhat: GNU Fortran (GCC 3.2.2 20030222 (Red Hat Linux 3.2.2-5)) 3.2.2 20030222 (Red Hat Linux 3.2.2-5)
-     # GNU Fortran (GCC) 3.4.2 (mingw-special)
+--- numpy/distutils/fcompiler/gnu.py.orig	2015-10-04 12:57:36 UTC
++++ numpy/distutils/fcompiler/gnu.py	2015-10-04 12:59:13
+@@ -83,7 +83,7 @@ class GnuFCompiler(FCompiler):
+             return None
+         return v[1]
  
 -    possible_executables = ['g77', 'f77']
 +    possible_executables = ['%%FC%%', 'g77', 'f77']
      executables = {
-         'version_cmd'  : [None, "--version"],
+         'version_cmd'  : [None, "-dumpversion"],
          'compiler_f77' : [None, "-g", "-Wall", "-fno-second-underscore"],
-@@ -72,7 +72,7 @@
+@@ -107,11 +107,13 @@ class GnuFCompiler(FCompiler):
          for key in ['version_cmd', 'compiler_f77', 'linker_so', 'linker_exe']:
              executables[key].append('-mno-cygwin')
  
 -    g2c = 'g2c'
 +    g2c = '%%FC%%'
- 
      suggested_f90_compiler = 'gnu95'
  
-@@ -295,7 +295,7 @@
-     #       GNU Fortran 95 (GCC) 4.2.0 20060218 (experimental)
-     #       GNU Fortran (GCC) 4.3.0 20070316 (experimental)
+     def get_flags_linker_so(self):
+         opt = self.linker_so[1:]
++        if 'FFLAGS' in os.environ:
++                opt.append(os.environ['FFLAGS'])
+         if sys.platform == 'darwin':
+             target = os.environ.get('MACOSX_DEPLOYMENT_TARGET', None)
+             # If MACOSX_DEPLOYMENT_TARGET is set, we simply trust the value
+@@ -265,7 +265,7 @@ class Gnu95FCompiler(GnuFCompiler):
+                     self.executables[key].append('-mno-cygwin')
+         return v
  
 -    possible_executables = ['gfortran', 'f95']
 +    possible_executables = ['%%FC%%', 'gfortran', 'f95']
      executables = {
-         'version_cmd'  : ["<F90>", "--version"],
-         'compiler_f77' : [None, "-Wall", "-ffixed-form",
+         'version_cmd'  : ["<F90>", "-dumpversion"],
+         'compiler_f77' : [None, "-Wall", "-g", "-ffixed-form",
