bb375c41374771cca042efc8c9f9ac60889b93cb
sambler <Develop@Shaneware.Biz>
2016-04-13 16:51:36 +0930

    new port py35-blender based on blender 2.77a

---
+++ graphics/py35-blender/files/patch-intern_opensubdiv_opensubdiv__converter.cc
@@ -0,0 +1,10 @@
+--- intern/opensubdiv/opensubdiv_converter.cc.orig	2015-10-15 02:21:24 UTC
++++ intern/opensubdiv/opensubdiv_converter.cc
+@@ -25,6 +25,7 @@
+ 
+ #include <cstdio>
+ #include <vector>
++#include <cstdlib>
+ 
+ #ifdef _MSC_VER
+ #  include "iso646.h"
