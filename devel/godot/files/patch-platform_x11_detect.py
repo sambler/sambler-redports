e3d3c388886cb42da506fa91c0c0e11e5143410c
sambler <Develop@Shaneware.Biz>
2018-01-30 18:20:47 +1030

    clang6 fixes

--- devel/godot/files/patch-platform_x11_detect.py
+++ devel/godot/files/patch-platform_x11_detect.py
@@ -1,18 +1,6 @@
---- platform/x11/detect.py.orig	2017-12-28 04:03:40 UTC
+--- platform/x11/detect.py.orig	2017-08-27 16:25:12 UTC
 +++ platform/x11/detect.py
-@@ -45,6 +45,11 @@ def can_build():
-         print("xrandr not found.. x11 disabled.")
-         return False
- 
-+    ssl_error = os.system("pkg-config openssl --modversion > /dev/null ")
-+    if (ssl_error):
-+        print("OpenSSL not found.. x11 disabled.")
-+        return False
-+
-     return True  # X11 enabled
- 
- 
-@@ -56,7 +61,9 @@ def get_opts():
+@@ -56,7 +56,9 @@ def get_opts():
          ('use_sanitizer', 'Use llvm compiler sanitize address', 'no'),
          ('use_leak_sanitizer', 'Use llvm compiler sanitize memory leaks', 'no'),
          ('use_lto', 'Use link time optimization', 'no'),
@@ -23,16 +11,24 @@
          ('udev', 'Use udev for gamepad connection callbacks', 'no'),
          ('debug_release', 'Add debug symbols to release version', 'no'),
      ]
-@@ -154,7 +161,7 @@ def configure(env):
+@@ -148,13 +150,14 @@ def configure(env):
+         # Currently not compatible with OpenSSL 1.1.0+
+         # https://github.com/godotengine/godot/issues/8624
+         import subprocess
+-        openssl_version = subprocess.check_output(['pkg-config', 'openssl', '--modversion']).strip('\n')
++        openssl_version = '1.0.2' #subprocess.check_output(['pkg-config', 'openssl', '--modversion']).strip('\n')
+         if (openssl_version >= "1.1.0"):
+             print("Error: Found system-installed OpenSSL %s, currently only supporting version 1.0.x." % openssl_version)
              print("Aborting.. You can compile with 'builtin_openssl=yes' to use the bundled version.\n")
              sys.exit(255)
  
 -        env.ParseConfig('pkg-config openssl --cflags --libs')
++        #env.ParseConfig('pkg-config openssl --cflags --libs')
 +        env.ParseConfig('echo -lssl -lcrypto')
  
      if (env['builtin_libwebp'] == 'no'):
          env.ParseConfig('pkg-config libwebp --cflags --libs')
-@@ -199,12 +206,13 @@ def configure(env):
+@@ -199,12 +202,13 @@ def configure(env):
      if (env['builtin_glew'] == 'no'):
          env.ParseConfig('pkg-config glew --cflags --libs')
  
@@ -52,15 +48,15 @@
  
      if (platform.system() == "Linux"):
          env.Append(CPPFLAGS=["-DJOYDEV_ENABLED"])
-@@ -219,6 +227,11 @@ def configure(env):
+@@ -218,6 +222,11 @@ def configure(env):
+             env.ParseConfig('pkg-config libudev --cflags --libs')
          else:
              print("libudev development libraries not found, disabling udev support")
- 
++
 +    if (env["oss"]=="yes"):
 +        print("Enabling OSS Audio")
 +        env.Append(CPPFLAGS=["-DOSS_ENABLED"])
 +        env.Append(CCFLAGS=['-Ithirdparty/rtaudio'])
-+
+ 
      if (env["pulseaudio"] == "yes"):
          if not os.system("pkg-config --exists libpulse-simple"):
-             print("Enabling PulseAudio")
