736a35fe3206ab0732934d084b1855e8fa8ea27f
sambler <Develop@Shaneware.Biz>
2018-02-01 23:00:50 +1030

    godot update to 3.0-stable

--- devel/godot/files/patch-platform_x11_detect.py
+++ devel/godot/files/patch-platform_x11_detect.py
@@ -1,62 +1,47 @@
---- platform/x11/detect.py.orig	2017-08-27 16:25:12 UTC
+--- platform/x11/detect.py.orig	2017-12-28 05:14:45 UTC
 +++ platform/x11/detect.py
-@@ -56,7 +56,9 @@ def get_opts():
-         ('use_sanitizer', 'Use llvm compiler sanitize address', 'no'),
-         ('use_leak_sanitizer', 'Use llvm compiler sanitize memory leaks', 'no'),
-         ('use_lto', 'Use link time optimization', 'no'),
--        ('pulseaudio', 'Detect & Use pulseaudio', 'yes'),
-+        ('alsa','Detect & Use alsa audio','no'),
-+        ('oss','Detect & Use OSS audio','no'),
-+        ('pulseaudio', 'Detect & Use pulseaudio', 'no'),
-         ('udev', 'Use udev for gamepad connection callbacks', 'no'),
-         ('debug_release', 'Add debug symbols to release version', 'no'),
-     ]
-@@ -148,13 +150,14 @@ def configure(env):
-         # Currently not compatible with OpenSSL 1.1.0+
-         # https://github.com/godotengine/godot/issues/8624
-         import subprocess
--        openssl_version = subprocess.check_output(['pkg-config', 'openssl', '--modversion']).strip('\n')
-+        openssl_version = '1.0.2' #subprocess.check_output(['pkg-config', 'openssl', '--modversion']).strip('\n')
-         if (openssl_version >= "1.1.0"):
-             print("Error: Found system-installed OpenSSL %s, currently only supporting version 1.0.x." % openssl_version)
-             print("Aborting.. You can compile with 'builtin_openssl=yes' to use the bundled version.\n")
-             sys.exit(255)
+@@ -52,7 +52,9 @@ def get_opts():
+         BoolVariable('use_static_cpp', 'Link stdc++ statically', False),
+         BoolVariable('use_sanitizer', 'Use LLVM compiler address sanitizer', False),
+         BoolVariable('use_leak_sanitizer', 'Use LLVM compiler memory leaks sanitizer (implies use_sanitizer)', False),
+-        BoolVariable('pulseaudio', 'Detect & use pulseaudio', True),
++        BoolVariable('alsa','Detect & Use alsa audio',False),
++        BoolVariable('oss','Detect & Use OSS audio',False),
++        BoolVariable('pulseaudio', 'Detect & Use pulseaudio', False),
+         BoolVariable('udev', 'Use udev for gamepad connection callbacks', False),
+         EnumVariable('debug_symbols', 'Add debug symbols to release version', 'yes', ('yes', 'no', 'full')),
+         BoolVariable('separate_debug_symbols', 'Create a separate file with the debug symbols', False),
+@@ -154,7 +156,7 @@ def configure(env):
+     # FIXME: Check for existence of the libs before parsing their flags with pkg-config
  
+     if not env['builtin_openssl']:
 -        env.ParseConfig('pkg-config openssl --cflags --libs')
-+        #env.ParseConfig('pkg-config openssl --cflags --libs')
 +        env.ParseConfig('echo -lssl -lcrypto')
  
-     if (env['builtin_libwebp'] == 'no'):
+     if not env['builtin_libwebp']:
          env.ParseConfig('pkg-config libwebp --cflags --libs')
-@@ -199,12 +202,13 @@ def configure(env):
-     if (env['builtin_glew'] == 'no'):
-         env.ParseConfig('pkg-config glew --cflags --libs')
+@@ -226,12 +228,18 @@ def configure(env):
  
--    if os.system("pkg-config --exists alsa") == 0:
+     ## Flags
+ 
+-    if (os.system("pkg-config --exists alsa") == 0): # 0 means found
 -        print("Enabling ALSA")
 -        env.Append(CPPFLAGS=["-DALSA_ENABLED"])
 -        env.ParseConfig('pkg-config alsa --cflags --libs')
 -    else:
 -        print("ALSA libraries not found, disabling driver")
-+    if (env["alsa"]=="yes"):
++    if env['alsa']:
 +        if os.system("pkg-config --exists alsa")==0:
 +            print("Enabling ALSA")
 +            env.Append(CPPFLAGS=["-DALSA_ENABLED"])
 +            env.Append(LIBS=['asound'])
 +        else:
 +            print("ALSA libraries not found, disabling driver")
- 
-     if (platform.system() == "Linux"):
-         env.Append(CPPFLAGS=["-DJOYDEV_ENABLED"])
-@@ -218,6 +222,11 @@ def configure(env):
-             env.ParseConfig('pkg-config libudev --cflags --libs')
-         else:
-             print("libudev development libraries not found, disabling udev support")
 +
-+    if (env["oss"]=="yes"):
++    if env['oss']:
 +        print("Enabling OSS Audio")
 +        env.Append(CPPFLAGS=["-DOSS_ENABLED"])
 +        env.Append(CCFLAGS=['-Ithirdparty/rtaudio'])
  
-     if (env["pulseaudio"] == "yes"):
-         if not os.system("pkg-config --exists libpulse-simple"):
+     if env['pulseaudio']:
+         if (os.system("pkg-config --exists libpulse-simple") == 0): # 0 means found
--- devel/godot/files/patch-platform_x11_os__x11.cpp
+++ devel/godot/files/patch-platform_x11_os__x11.cpp
@@ -1,11 +1,13 @@
---- platform/x11/os_x11.cpp.orig	2018-01-10 10:08:3 UTC
+--- platform/x11/os_x11.cpp.orig	2018-01-03 04:32:19 UTC
 +++ platform/x11/os_x11.cpp
-@@ -1243,7 +1243,7 @@ static Property read_property(Display *p
+@@ -2469,6 +2469,10 @@ OS_X11::OS_X11() {
+ 	AudioDriverManager::add_driver(&driver_pulseaudio);
+ #endif
  
- 	} while (bytes_after != 0);
- 
--	Property p = { ret, actual_format, nitems, actual_type };
-+	Property p = { ret, actual_format, (int)nitems, actual_type };
- 
- 	return p;
- }
++#ifdef RTAUDIO_ENABLED
++	AudioDriverManager::add_driver(&driver_rtaudio);
++#endif
++
+ #ifdef ALSA_ENABLED
+ 	AudioDriverManager::add_driver(&driver_alsa);
+ #endif
