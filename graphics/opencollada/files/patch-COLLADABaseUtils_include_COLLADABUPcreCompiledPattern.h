4ef1ae355d19a5236fbdb28d957683ac7dd96570
sambler <Develop@Shaneware.Biz>
2018-06-01 20:16:36 +0930

    opencollada update to 1.6.62

---
+++ graphics/opencollada/files/patch-COLLADABaseUtils_include_COLLADABUPcreCompiledPattern.h
@@ -0,0 +1,13 @@
+--- COLLADABaseUtils/include/COLLADABUPcreCompiledPattern.h.orig	2018-06-01 01:57:47 UTC
++++ COLLADABaseUtils/include/COLLADABUPcreCompiledPattern.h
+@@ -13,8 +13,8 @@
+ 
+ #include "COLLADABUPrerequisites.h"
+ 
+-struct real_pcre;
+-typedef struct real_pcre pcre;
++struct real_pcre8_or_16;
++typedef struct real_pcre8_or_16 pcre;
+ 
+ 
+ namespace COLLADABU
