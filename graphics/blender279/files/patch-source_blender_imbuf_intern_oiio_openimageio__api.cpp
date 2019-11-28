14820f688e437f1979fab54159fc499b3bfba60b
sambler <Develop@Shaneware.Biz>
2019-11-28 13:56:09 +1030

    update blender279 and add oiio18 for it to use

--- graphics/blender279/files/patch-source_blender_imbuf_intern_oiio_openimageio__api.cpp
+++ graphics/blender279/files/patch-source_blender_imbuf_intern_oiio_openimageio__api.cpp
@@ -1,4 +1,4 @@
---- source/blender/imbuf/intern/oiio/openimageio_api.cpp.orig	2016-09-28 03:53:25 UTC
+--- source/blender/imbuf/intern/oiio/openimageio_api.cpp.orig	2018-02-20 09:26:29 UTC
 +++ source/blender/imbuf/intern/oiio/openimageio_api.cpp
 @@ -35,6 +35,9 @@
  #include "utfconv.h"
@@ -10,13 +10,13 @@
  extern "C"
  {
  #include "MEM_guardedalloc.h"
-@@ -48,9 +51,6 @@ extern "C"
+@@ -47,9 +50,6 @@ extern "C"
+ #include "IMB_colormanagement.h"
  #include "IMB_colormanagement_intern.h"
  }
- 
+-
 -#include <openimageio_api.h>
 -#include <OpenImageIO/imageio.h>
--
+ 
  OIIO_NAMESPACE_USING
  
- using std::string;
